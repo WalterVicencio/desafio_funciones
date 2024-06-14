c8bd7c8699cebb3c1160fc346e2879a96d008b4a
Walteriano <walter.8128@gmail.com>
2024-06-13 21:02:58 -0400

    primera_etapa_lista

---
+++ 1_funcion.js
@@ -0,0 +1,13 @@
+function example (a,b,c){
+    return a+b+c
+}
+
+console.log(example(1,2,3))
+
+//PASANDO A UNA FUNCION DE EXPRESION-->
+
+expresion_example = function(a,b,c){
+    return a+b+c
+}
+
+console.log(expresion_example(1,2,3))
---
+++ 2_arrow.js
@@ -0,0 +1,10 @@
+let suma = function(a,b){
+    return a + b
+}
+console.log(suma(3,4))
+
+//PASANDO A UNA ARROW FUNCION-->
+
+arrow_suma = (a,b) => a + b
+
+console.log(arrow_suma(3,4))
