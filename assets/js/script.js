842e33d131d227e97641a4afc5d76176360ad302
Walteriano <walter.8128@gmail.com>
2024-06-13 21:18:58 -0400

    segunda_etapa_lista

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+
+const ele = document.getElementById("ele1")
+let color = 'green'
+
+pintar = (color) => ele.style.backgroundColor = color
+pintar(color)
+ele.addEventListener("click", () => ele.style.backgroundColor = 'yellow');
